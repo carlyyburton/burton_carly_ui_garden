c7c8ebaa7fc388910ca3c85ead5ab98010588b5b
carlyyburton <carlyyburton@gmail.com>
2023-10-30 14:13:13 -0500

    Button component finished.

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "UI-Garden/Button",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = (args) => (
+  <Button data-test-id="InputField-id" {...args} />
+);
+Primary.args = {
+  primary: true,
+  disabled: false,
+  text: "Enabled",
+};
+
+export const Disabled: Story = (args) => (
+  <Button data-test-id="InputField-id" {...args} />
+);
+Disabled.args = {
+  primary: false,
+  disabled: true,
+  text: "Disabled",
+};
---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Button from "./Button";
+
+describe("Running Test #1", () => {
+  test("Check Button Visible", () => {
+    render(<Button text="Button" />);
+    expect(
+      screen.getByRole("button", { name: "Button" })
+    ).toBeVisible();
+  });
+});
+
+describe("Running Test #2", () => {
+  test("Check Button Disabled", () => {
+    render(<Button text="Button" disabled />);
+    expect(
+      screen.getByRole("button", { name: "Button" })
+    ).toBeDisabled();
+  });
+});
